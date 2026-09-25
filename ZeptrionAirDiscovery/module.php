fa0d3a4cd94c565465b8643cd47309659e8991b6
mb-stern <95777848+mb-stern@users.noreply.github.com>
2026-09-25 19:22:57 +0200

    Neue Geräte mit Recall Erreichbar und RSSI anlegen

--- ZeptrionAirDiscovery/module.php
+++ ZeptrionAirDiscovery/module.php
@@ -44,16 +44,20 @@ class ZeptrionAirDiscovery extends IPSModuleStrict
                             'Channels'     => $device['channels'],
                             'Channel1Name'  => $device['channelConfig'][1]['name'] ?? 'Kanal 1',
                             'Channel1Type'  => $device['channelConfig'][1]['type'] ?? 'unused',
-                            'Channel1Scenes'=> $device['channelConfig'][1]['scenes'] ?? false,
                             'Channel2Name'  => $device['channelConfig'][2]['name'] ?? 'Kanal 2',
                             'Channel2Type'  => $device['channelConfig'][2]['type'] ?? 'unused',
-                            'Channel2Scenes'=> $device['channelConfig'][2]['scenes'] ?? false,
                             'Channel3Name'  => $device['channelConfig'][3]['name'] ?? 'Kanal 3',
                             'Channel3Type'  => $device['channelConfig'][3]['type'] ?? 'unused',
-                            'Channel3Scenes'=> $device['channelConfig'][3]['scenes'] ?? false,
                             'Channel4Name'  => $device['channelConfig'][4]['name'] ?? 'Kanal 4',
                             'Channel4Type'  => $device['channelConfig'][4]['type'] ?? 'unused',
-                            'Channel4Scenes'=> $device['channelConfig'][4]['scenes'] ?? false
+                            'ShowOnline' => true,
+                            'ShowRSSI' => true,
+                            'ShowScenes' => true,
+                            'ShowIPAddress' => false,
+                            'ShowDeviceTypeInfo' => false,
+                            'ShowSerialNumberInfo' => false,
+                            'ShowSoftwareInfo' => false,
+                            'ShowChannelActualValues' => false
                         ],
                         // Instanzname immer anhand des eindeutigen zapp-Hostnamens setzen.
                         'name' => $host
